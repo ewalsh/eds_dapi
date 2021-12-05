ee12005ca7f2a9e0bcacdf6ac25fd6ddb76f586b
Edmund <ewalsh@economicdatasciences.ai>
2021-12-05 17:54:42 +0000

    updated readme and env settings

--- README.md
+++ README.md
@@ -7,8 +7,10 @@ docker-compose up --build
 # zeppelin link
 http://localhost:8087
 
-# updates to zeppelin
+# updates to zeppelin and django
 1. create a folder at the root named `zdata`
   - I have added to the docker-compose to share data with the container
   - I have also added seaborn and statsmodels
-<!-- 2. In future, if you want  -->
+2. To make sure you have the latest docker images please run:
+  - docker pull ewalsh200/python-nginx
+  - docker pull ewalsh200/zeppelin:focal
--- dapi/dapi/settings.py
+++ dapi/dapi/settings.py
@@ -24,12 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = = os.getenv('SECRET_KEY', 'hidden')
+SECRET_KEY = os.getenv('SECRET_KEY', 'hidden')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.getenv('MODE', 'production') == 'production':
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = []
+else:
+    DEBUG = True
+
+    ALLOWED_HOSTS = []
 
 
 # Application definition
