327b5f5869b7d72f963532a1a6dd9b845e3ef454
Edmund <ewalsh@economicdatasciences.ai>
2022-02-27 09:32:27 +0000

    added benchmark assets by class and generic eds data function

--- dapi/dapi/eds_api.py
+++ dapi/dapi/eds_api.py
@@ -20,6 +20,18 @@ class EDSsession():
         self.session.post(
             'https://economicdatasciences.com/login', data=self.payload)
 
+    # general data function -- personally, I don't love this although
+    # it is less code. Simply, I won't always remember the url setup,
+    # so I prefer functions with clear, consistent names
+    def get_eds_data(self, *args):
+        url_str = 'https://economicdatasciences.com'
+        assert(len(args) > 0), "Must include url arguments"
+        for s in args:
+            url_str = url_str + '/' + s
+
+        res = self.session.get(url_str)
+        return(res.json())
+
     # benchmark
     def get_benchmark(self, code):
         get_str = 'https://economicdatasciences.com/benchmarks/' + code
@@ -41,5 +53,11 @@ class EDSsession():
     # get base factors
     def get_factormap(self):
         get_str = 'https://economicdatasciences.com/factormap'
+        res = self.session.get(get_str)
+        return(res.json())
+
+    # get asset holdings for a benchmark
+    def get_benchmark_assets_by_class(self, asset_class):
+        get_str = 'https://economicdatasciences.com/assets-by-class/' + asset_class
         res = self.session.get(get_str)
         return(res.json())
